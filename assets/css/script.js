5a7a71bb0812de32fc779a46777d4c2c091f8309
srilakshminarasimha3925 <srilakshminarasimha3925@gmail.com>
2026-05-24 15:45:44 +0530

    Create script.js

---
+++ assets/css/script.js
@@ -0,0 +1,21 @@
+// Optional: Highlight active menu while scrolling
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 120) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(a => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === "#" + current) {
+      a.classList.add("active");
+    }
+  });
+});
